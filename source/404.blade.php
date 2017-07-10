c48d2e1a63344332d91e32034a58beca4337b6a6
Jonathan Torres <jonathantorres41@gmail.com>
2017-07-09 22:41:39 -0400

    Add 404 page.

---
+++ source/404.blade.php
@@ -0,0 +1,5 @@
+@extends('_layouts.master')
+
+@section('body')
+    <p class="text-center">I'm sorry. I couldn't find what you were looking for. <br>Go back to the <a href="http://www.jonathantorres.com">home page</a>. I'm sure you'll find something more interesting there ;)</p>
+@endsection
