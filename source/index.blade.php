b54a9acc57fe730e918fa9a6198d062cdd666549
Jonathan Torres <jonathantorres41@gmail.com>
2017-07-18 21:23:19 -0400

    Layout changes on the home page.

--- source/index.blade.php
+++ source/index.blade.php
@@ -15,12 +15,6 @@
     <hr>
 
     <div class="row ">
-        <div class="col-md-6 writing">
-            <h1>Writing</h1>
-            <ul class="list-unstyled posts-list">
-            </ul>
-        </div>
-
         <div class="col-md-6 projects">
             <h1>Projects</h1>
             <ul class="list-unstyled posts-list">
@@ -38,23 +32,21 @@
                 </li>
             </ul>
         </div>
-    </div>
-
-    <hr>
-
-    <div class="row about">
-        <div class="col-md-12">
-            <h1>Who I am?</h1>
-            <p>I was born and raised in Puerto Rico but I'm currently living in sunny Miami, FL. I enjoy playing basketball, running, sci-fi movies and video games.</p>
+        <div class="col-md-6">
+            <div class="row about">
+                <div class="col-md-12">
+                    <h1>Who I am?</h1>
+                    <p>I was born and raised in Puerto Rico but I'm currently living in sunny Miami, FL. I enjoy playing basketball, running, sci-fi movies and video games.</p>
+                </div>
+            </div>
+            <div class="row contact">
+                <div class="col-md-12">
+                    <h1>Get in touch</h1>
+                    <p>Drop me a line at <a href="mailto:jonathantorres41@gmail.com">jonathantorres41@gmail.com</a></p>
+                </div>
+            </div>
         </div>
     </div>
 
     <hr>
-
-    <div class="row contact">
-        <div class="col-md-12">
-            <h1>Get in touch</h1>
-            <p>Drop me a line at <a href="mailto:jonathantorres41@gmail.com">jonathantorres41@gmail.com</a></p>
-        </div>
-    </div>
 @endsection
