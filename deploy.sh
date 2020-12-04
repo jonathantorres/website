65a3829edb22d62f651eec4fb7ce151b62ac78ca
Jonathan Torres <jonathantorres41@gmail.com>
2020-12-04 15:49:44 -0500

    Updates on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
-git pull origin master
-npm install
-# Compile Sass
-npm run sass
+#!/usr/bin/env bash
+
+if [ -d "/var/www/jonathantorres/public" ]; then
+    sudo rm -rf /var/www/jonathantorres/public
+fi
+
+# copy folders and files
+sudo cp -vr public/ /var/www/jonathantorres
+
+# change permissions and ownership
+sudo chown -R www-data:www-data /var/www/jonathantorres
+find /var/www/jonathantorres/ -type d -exec sudo chmod 755 '{}' \;
+find /var/www/jonathantorres/ -type f -exec sudo chmod 644 '{}' \;
